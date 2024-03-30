01041e30a0480df0358dadff20d7b7e733eeae28
Bool-Pug <jswwolf120@gmail.com>
2024-03-30 09:31:29 -0500

    updated stuff to work with lvl 3 gearing Added and renamed autos increased current limit on the vortexes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -99,7 +99,7 @@ public final class Constants {
 
    public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
-  public static final double ROBOT_MAX_SPEED = Units.feetToMeters(19.3);
+  public static final double ROBOT_MAX_SPEED = Units.feetToMeters(17.6);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
   public static final double FIELD_WIDTH = Units.inchesToMeters(653.2);  // (76.1 + 250.5 ) * 2 = 653.2 inches
 
